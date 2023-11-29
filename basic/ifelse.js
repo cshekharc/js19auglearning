fbbb410d26180022f38b909f7d5c8d7df0488049
cshekharc <cshekhars.choudhari@gmail.com>
2023-11-29 06:46:19 +0530

    if else and loops

---
+++ basic/ifelse.js
@@ -0,0 +1,28 @@
+// if else conditions
+
+// let age = 18
+// if (age >= 18){
+//     console.log("Person is eligible for voting")
+// //}
+// }else if(age === 18){
+//     console.log("Person is newly eligible for voting")   
+// }
+// else{
+//     console.log("Person is not eligible for voting")
+// }
+
+//switch statement
+let num = 10
+switch(num){
+    case 1:
+        console.log("we are in case 1")
+        break
+    case 2:
+        console.log("we are in case 2")
+        break
+    case 15:
+        console.log("we are in case 15")
+        break
+    default:
+        console.log("No case available")
+}
---
+++ basic/loops.js
@@ -0,0 +1,70 @@
+// do while loop
+
+// let i =1
+// do{
+//     console.log(i)
+//     i++
+// }while(i>=5)
+
+// while loop
+// let i = 1
+// while (i<=5){
+//     console.log(i)
+//     i++
+// }
+
+//for loop
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+// }
+
+// for...in loop -> when we are dealing with object
+// let courses = {
+//     tech : "ATT",
+//     fees : 30000,
+//     location: "online",
+//     batch : "19 Aug"
+// }
+
+// for (let course in courses) {
+//     console.log(courses[course])
+//     // if(courses[course] === "ATT"){
+//     //     console.log(courses.fees)
+//     // }
+// }
+
+//for...of -> when we are dealing with array
+
+// let arr =  [4, 10, "abc", true]
+
+// for(let ar of arr){
+//     if(ar === 10){
+//         console.log('name is there in list')
+//         break
+//     }else if(ar!= 10){
+//         console.log('name is not there in list')
+//         break
+//     }else{
+//         continue
+//     }
+// }
+
+let arr =  [4, 10, "abc", true];
+
+let nameFound = false;
+
+for (let ar of arr) {
+    if (ar === 10) {
+        console.log('name is there in list');
+        nameFound = true;
+        break;
+    } else {
+        // No need to use else if for this scenario
+        // If ar is not equal to 10, it will reach here
+        continue;
+    }
+}
+
+if (!nameFound) {
+    console.log('name is not there in list');
+}
